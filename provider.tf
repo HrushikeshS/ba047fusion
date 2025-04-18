c98dd21a77fb81955cc1048956971908ed60fa01
Hrushi Salgaonkar <salgaonkar.hrushi@gmail.com>
2025-04-18 21:24:38 +0530

    Added VPC in provider block

--- provider.tf
+++ provider.tf
@@ -2,7 +2,11 @@ provider "aws" {
     region = "us-west-2"
   
 }
-resourse "aws_vpc" "myVPC" {
-  cidr_block = "10.10.0.0/16"
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
+  tags = {
+    Name = "main"
+  }
 }
